1dd36b7cc9b20b833d3cc0cae2d37bffb83b1f57
Cole Pergerson <cole.pergerson@gmail.com>
2023-09-10 15:18:03 -0700

    Modified setup files

--- Source/main.lua
+++ Source/main.lua
@@ -1,8 +1,8 @@
 local gfx = playdate.graphics
 
-gfx.setColor(gfx.kColorBlack)
+--gfx.setColor(gfx.kColorBlack)
 
 function playdate.update()
-    gfx.fillRect(0, 0, 400, 240)
+    --gfx.fillRect(0, 0, 400, 240)
     playdate.drawFPS(0,0)
 end
